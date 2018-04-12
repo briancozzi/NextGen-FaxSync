f5d1e4aa1f96cfa25b671713e5a748b3636329bf
Marko Mitreski <marko.mitreski@gmail.com>
2018-04-11 23:37:31 -0500

    Fix namespaces

--- FaxSync.Domain/ActionSync.cs
+++ FaxSync.Domain/ActionSync.cs
@@ -1,5 +1,4 @@
-﻿using FaySync.Models;
-using FaxSync.Models;
+﻿using FaxSync.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- FaxSync.Domain/Attorney.cs
+++ FaxSync.Domain/Attorney.cs
@@ -1,9 +1,9 @@
-﻿using FaxSync.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FaxSync.Models;
 
 namespace FaxSync.Domain
 {
